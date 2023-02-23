97a08bdf04f4daf5995eb43a273d230454268069
eternasuno <eternasuno@icloud.com>
2023-02-23 04:51:27 +0000

    init

---
+++ pages/api/audio/bilibili/[bvid].ts
@@ -0,0 +1,66 @@
+import { type NextRequest } from "next/server";
+
+export const config = {
+    runtime: "edge",
+};
+
+const getInfo = async (bvid: string) => {
+    const url = new URL("https://api.bilibili.com/x/web-interface/view");
+    url.searchParams.append("bvid", bvid);
+
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
+    const { code, message, data } = await response.json();
+    if (code !== 0) {
+        throw new Error(message);
+    }
+
+    return data;
+};
+
+const getAudioPath = async (bvid: string, cid: string) => {
+    const url = new URL("https://api.bilibili.com/x/player/playurl");
+    url.searchParams.append("bvid", bvid);
+    url.searchParams.append("cid", cid);
+    url.searchParams.append("fnval", "16");
+
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
+    const {
+        code,
+        message,
+        data: {
+            dash: { audio },
+        },
+    } = await response.json();
+    if (code !== 0) {
+        throw new Error(message);
+    }
+
+    return audio[0].baseUrl as string;
+};
+
+const handle = async (request: NextRequest) => {
+    const { searchParams } = new URL(request.url);
+    const bvid = searchParams.get("bvid")!;
+
+    try {
+        const { cid } = await getInfo(bvid);
+        const path = await getAudioPath(bvid, cid);
+        return new Response(JSON.stringify({ path }));
+    } catch (error: any) {
+        const message = error.message;
+        return new Response(JSON.stringify({ message }), {
+            status: 500,
+            statusText: "Internal Server Error",
+        });
+    }
+};
+
+export default handle;
