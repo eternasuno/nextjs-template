25881e7e46cce1ed05cb4bd8af75e7eaeab85928
eternasuno <eternasuno@icloud.com>
2023-02-23 10:38:50 +0000

    [feature] add README

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Index = () => {
+    return (
+        <div>
+            <p>Convert Bilibili to podcast.</p>
+            <p>
+                example: <Link href="/bilibili/user/70666">舍长驾到</Link>
+            </p>
+        </div>
+    );
+};
+
+export default Index;
