c4514516f9f64bf81ab89578b28a1448e9b72a61
eternasuno <eternasuno@icloud.com>
2023-10-14 11:57:45 +0000

    [fix] url ttl

--- lib/middlewares/with-sound.ts
+++ lib/middlewares/with-sound.ts
@@ -6,8 +6,11 @@ import { md5 } from '../crypto';
 export const withSound =
     <T>(handle: Handle<T, string>): Handle<T, Response> =>
     async (request, ctx) => {
-        const url = await cache.wrap(md5(request.url), async () =>
-            handle(request, ctx)
+        const url = await cache.wrap(
+            md5(request.url),
+            async () => handle(request, ctx),
+            // url will expire after 2 hours
+            2 * 60 * 60 * 1000
         );
 
         return NextResponse.redirect(url, {
