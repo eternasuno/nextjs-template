d607ee49bcc3aebbf40ba29ab247bc0cb93ba0ea
eternasuno <eternasuno@icloud.com>
2023-10-14 13:09:03 +0000

    [fix] add log

--- lib/cache/redis.ts
+++ lib/cache/redis.ts
@@ -10,6 +10,7 @@ const createCache = async () => {
 
     const match = /:\/\/\S*@(\S+):\d+/.exec(config.redis.url);
     const servername = (match && match.at(1)) || undefined;
+    console.info(servername);
 
     return caching(
         redisInsStore(
