225a50ed2eccd8e2b0c982b0057dc283ed196a3f
eternasuno <eternasuno@icloud.com>
2023-10-16 08:48:47 +0000

    [feat] Add index page

---
+++ app/layout.tsx
@@ -0,0 +1,8 @@
+const Layout = ({ children }: { children: React.ReactNode }) => (
+    <html lang="en">
+        <head />
+        <body>{children}</body>
+    </html>
+);
+
+export default Layout;
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+const Page = () => <p>Conver bilbili to podcast.</p>;
+
+export default Page;
