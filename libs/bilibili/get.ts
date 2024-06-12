4b0639cbadfc8ec816dfe541d5bcf49033d4cf90
eternasuno <22316214+eternasuno@users.noreply.github.com>
2024-06-12 02:58:30 +0000

    [fix] more ua

--- libs/bilibili/get.ts
+++ libs/bilibili/get.ts
@@ -6,6 +6,9 @@ const userAgents = [
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:124.0) Gecko/20100101 Firefox/124.0',
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:127.0) Gecko/20100101 Firefox/127.0',
 ];
 
 export const get = retry(
@@ -13,7 +16,7 @@ export const get = retry(
     const response = await fetch(url, {
       headers: {
         'user-agent': getRandomItem(userAgents),
-        referer: 'https://space.bilibili.com',
+        referer: 'https://www.bilibili.com',
       },
     });
 
