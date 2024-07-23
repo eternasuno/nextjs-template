0b2bf2c50bfe756c3535720fe2b267c56606c886
eternasuno <22316214+eternasuno@users.noreply.github.com>
2024-07-23 09:03:34 +0000

    [fix] video path can not expired

--- libs/bilibili/api.ts
+++ libs/bilibili/api.ts
@@ -1,5 +1,5 @@
 import { getWithWbi } from './get.ts';
-import { tryGetLong, tryGetShort } from '../cache.ts';
+import { tryGetLong } from '../cache.ts';
 
 export const getUserInfo = tryGetLong(
   async (id: string) => {
@@ -32,21 +32,18 @@ export const getUserVideoList = async (id: string, limit: number, keyword?: stri
   return Promise.all(vlist.map(({ bvid }) => getVideoInfo(bvid)));
 };
 
-export const getVideoPath = tryGetShort(
-  async (bvid: string, cid: string) => {
-    type Data = { durl: [{ url: string }] };
+export const getVideoPath = async (bvid: string, cid: string) => {
+  type Data = { durl: [{ url: string }] };
 
-    const url = new URL('https://api.bilibili.com/x/player/playurl');
-    url.searchParams.append('bvid', bvid);
-    url.searchParams.append('cid', cid);
-    url.searchParams.append('platform', 'html5');
+  const url = new URL('https://api.bilibili.com/x/player/playurl');
+  url.searchParams.append('bvid', bvid);
+  url.searchParams.append('cid', cid);
+  url.searchParams.append('platform', 'html5');
 
-    const { durl: [{ url: path }] } = await getWithWbi<Data>(url);
+  const { durl: [{ url: path }] } = await getWithWbi<Data>(url);
 
-    return path;
-  },
-  'video_path',
-);
+  return path;
+};
 
 const getVideoInfo = tryGetLong(
   async (id: string) => {
