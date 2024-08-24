fca80333dad7a6ecd7253d7cd20a6747b30e254d
eternasuno <22316214+eternasuno@users.noreply.github.com>
2024-08-24 02:50:38 +0000

    [refactor] add expire time in cache

--- libs/cache.ts
+++ libs/cache.ts
@@ -2,6 +2,11 @@ const CACHE_LONG_TERM = Number(Deno.env.get('CACHE_LONG_TERM')) || 43_200_000;
 const CACHE_SHORT_TREM = Number(Deno.env.get('CACHE_SHORT_TREM')) || 3_600_000;
 const STORE = Deno.env.get('DENO_ENV') !== 'development';
 
+type Value<R> = {
+  data: R;
+  expireAt: number;
+};
+
 export const tryGet = <T extends unknown[], R>(
   get: (...args: T) => R | Promise<R>,
   key: string,
@@ -12,11 +17,16 @@ export const tryGet = <T extends unknown[], R>(
 async (...args: T) => {
   const argsKey = btoa(JSON.stringify(args));
   const cache = await Deno.openKv();
-  const { value } = await cache.get<R>([key, argsKey]);
-  const result = value || await get(...args);
+  const { value } = await cache.get<Value<R>>([key, argsKey]);
+  const hit = value && value.expireAt > Date.now();
+  const result = hit ? value.data : await get(...args);
 
-  if ((store && !value) || (refresh && value)) {
-    await cache.set([key, argsKey], result, { expireIn });
+  if ((store && !hit) || (refresh && hit)) {
+    await cache.set(
+      [key, argsKey],
+      { data: result, expireAt: Date.now() + expireIn },
+      { expireIn },
+    );
   }
 
   return result;
