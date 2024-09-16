ef1b31d90ba19096737a4bdad55a50002c53c9b7
eternasuno <22316214+eternasuno@users.noreply.github.com>
2024-09-16 05:07:39 +0000

    [fix] remove cache

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
 import '@/utils/env.ts';
 
-import { CACHE_ENABLE, CACHE_SHORT_TREM, PORT, TOKEN } from '@/config.ts';
+import { PORT, TOKEN } from '@/config.ts';
 import bilibili from '@/routes/bilibili.ts';
 import { Hono } from '@hono/hono';
-import { cache } from '@hono/hono/cache';
 import { logger } from '@hono/hono/logger';
 import { validator } from '@hono/hono/validator';
 import { RetryError } from '@std/async';
@@ -19,12 +18,6 @@ app.use(validator('query', (value, context) => {
   }
 
   return value;
-}));
-
-CACHE_ENABLE && app.use(cache({
-  cacheName: 'podcast',
-  cacheControl: `max-age=${Math.floor(CACHE_SHORT_TREM / 1000)}`,
-  wait: true,
 }));
 
 app.onError((error, context) => {
